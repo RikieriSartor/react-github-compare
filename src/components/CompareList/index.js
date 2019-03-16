1567b75652964560850f1409a259706b6c057508
Rikieri Sartor <riki_eri@hotmail.com>
2019-03-16 11:15:47 -0300

    Adicionando axios e chamada a API github

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,35 +1,55 @@
 import React from 'react';
-import { Container, Repository } from './styles';
-// https://api.github.com/repos/
-const CompareList = () => (
-  <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-      </header>
+import PropTypes from 'prop-types';
 
-      <ul>
-        <li>
-          95019
-          <small>stars</small>
-        </li>
-        <li>
-          95019
-          <small>forks</small>
-        </li>
-        <li>
-          95019
-          <small>issues</small>
-        </li>
-        <li>
-          95019
-          <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+import { Container, Repository } from './styles';
+
+const CompareList = ({ repositories }) => (
+  <Container>
+    { repositories.map(repository => (
+      <Repository key={repository.id}>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
+
+        <ul>
+          <li>
+            {repository.stargazers_count}
+            <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count}
+            <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count}
+            <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at}
+            <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    )) }
+
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    owner: PropTypes.shape({
+      login: PropTypes.string,
+      avatar_url: PropTypes.string,
+    }),
+    stargazers_count: PropTypes.number,
+    forks_count: PropTypes.number,
+    open_issues: PropTypes.number,
+    pushed_ad: PropTypes.string,
+  })).isRequired,
+};
 
 export default CompareList;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../assets/logo.png';
 import { Container, Form } from './styles';
 import CompareList from '../../components/CompareList';
+import api from '../../services/api';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
 
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
+  handleAddRepository = async (e) => {
+    e.preventDefault();
 
-    <CompareList />
-  </Container>
-);
+    try {
+      const response = await api.get(`/repos/${ this.state.repositoryInput }`);
 
-export default Main;
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, response.data],
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={ this.state.repositoryInput }
+            onChange={e => this.setState({ repositoryInput: e.target.value })}/>
+          <button type="submit">OK</button>
+        </Form>
+
+        <CompareList repositories={ this.state.repositories } />
+      </Container>
+    );
+  }
+}
